b329ee18e84af1c008c19282aa9b83e74da5fc23
RokkoVehkkonen <guido.alessandro@yahoo.it>
2016-06-18 11:04:14 +0200

    scores list added
    now we can show the scorelist sorted by score.

--- js/ScoresController.js
+++ js/ScoresController.js
@@ -1,5 +1,20 @@
-   
+     
    wordPuzzleApp.controller('ScoresController', function($scope, $http) {
+         
+      $scope.scores = null;
+
+      function getScores(){
+                
+         var qs = 'http://www.techlinx.info/scorefeed.php?command=getScores';       
+                
+         $http.get(qs)
+          .then(function(response) {
+            $scope.scores = response.data;
+       });
+        
+      } 
+      
+      getScores();   
           
   });
      
